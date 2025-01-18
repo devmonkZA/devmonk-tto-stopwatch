de75b33dff512b6c5483fa95c382bb7c57d8b6a1
Devmonk <thedevmonk@gmail.com>
2025-01-18 11:57:05 +0200

    Update 8

--- src/project.v
+++ src/project.v
@@ -17,11 +17,11 @@ module tt_um_devmonk_stopwatch (
 );
 
 //All output pins must be assigned. If not used, assign to 0.
-assign uio_out = 0;
-assign uio_in = 0;
-assign uio_oe  = 0;
-assign ena = 0;
-assign rst_n = 0;
+//assign uio_out = 0;
+//assign uio_in = 0;
+//assign uio_oe  = 0;
+//assign ena = 0;
+//assign rst_n = 0;
 
 // 7 segment control line bus
 wire [7:0] seven_segment;
